807bb3e3122908eb1da973d920141783b0c94804
devSomethingSomething <61376074+devSomethingSomething@users.noreply.github.com>
2021-06-02 18:28:24 +0200

    Refactor main method and add new class with pizza simulation

--- dart_async_practice/lib/main.dart
+++ dart_async_practice/lib/main.dart
@@ -1,48 +1,7 @@
 // import 'dart:io';
 
+import 'package:dart_async_practice/pizza_simulation.dart';
+
 void main() {
-  goEatPizza();
-}
-
-void goEatPizza() async {
-  order();
-  // var pizza = await getPizzaOrdered();
-  getPizzaOrdered().then((value) {
-    eatAndPay(value);
-  });
-  chatting();
-  // eatAndPay(pizza);
-}
-
-void order() {
-  print('Done ordering pizza');
-}
-
-Future<String> getPizzaOrdered() async {
-  var pizza = 'No pizza yet';
-
-  // sleep(Duration(seconds: 3));
-
-  await Future.delayed(
-    Duration(seconds: 3),
-    () {
-      pizza = 'Triple C';
-
-      print('Pizza delivered successfully');
-    },
-  );
-
-  // return pizza;
-
-  return Future<String>.value(pizza);
-}
-
-void chatting() {
-  print('Chatting...');
-}
-
-void eatAndPay(String pizza) {
-  // sleep(Duration(seconds: 3));
-
-  print('Paid for $pizza and on my way home');
+  PizzaSimulation().goEatPizza();
 }
---
+++ dart_async_practice/lib/pizza_simulation.dart
@@ -0,0 +1,49 @@
+class PizzaSimulation {
+  PizzaSimulation();
+
+  void goEatPizza() async {
+    order();
+
+    // var pizza = await getPizzaOrdered();
+
+    getPizzaOrdered().then((value) {
+      eatAndPay(value);
+    });
+    chatting();
+
+    // eatAndPay(pizza);
+  }
+
+  void order() {
+    print('Done ordering pizza');
+  }
+
+  Future<String> getPizzaOrdered() async {
+    var pizza = 'No pizza yet';
+
+    // sleep(Duration(seconds: 3));
+
+    await Future.delayed(
+      Duration(seconds: 3),
+      () {
+        pizza = 'Triple C';
+
+        print('Pizza delivered successfully');
+      },
+    );
+
+    // return pizza;
+
+    return Future<String>.value(pizza);
+  }
+
+  void chatting() {
+    print('Chatting...');
+  }
+
+  void eatAndPay(String pizza) {
+    // sleep(Duration(seconds: 3));
+
+    print('Paid for $pizza and on my way home');
+  }
+}
