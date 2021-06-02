6a76f848a91fe59b117708af614a325aef576872
devSomethingSomething <61376074+devSomethingSomething@users.noreply.github.com>
2021-06-02 17:28:45 +0200

    Add main method and remove unused files

--- dart_async_practice/bin/dart_async_practice.dart
+++
@@ -1,5 +0,0 @@
-import 'package:dart_async_practice/dart_async_practice.dart' as dart_async_practice;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_async_practice.calculate()}!');
-}
