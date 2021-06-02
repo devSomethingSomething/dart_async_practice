e74741d90b8d1a9030cb1393023e39ddafddd11c
devSomethingSomething <61376074+devSomethingSomething@users.noreply.github.com>
2021-06-02 19:40:23 +0200

    Add code for reading online JSON data

--- dart_async_practice/lib/main.dart
+++ dart_async_practice/lib/main.dart
@@ -1,7 +1,27 @@
 // import 'dart:io';
 
-import 'package:dart_async_practice/pizza_simulation.dart';
+// import 'package:dart_async_practice/pizza_simulation.dart';
 
-void main() {
-  PizzaSimulation().goEatPizza();
+import 'dart:convert';
+
+import 'package:http/http.dart';
+
+void main() async {
+  // PizzaSimulation().goEatPizza();
+
+  var data = await fetchData();
+
+  print(data);
+}
+
+Future<Map<String, dynamic>> fetchData() async {
+  final response = await get(
+    Uri.parse('https://dl.dropbox.com/s/3lh91mqoesofttl/test.json?dl=0'),
+  );
+
+  if (response.statusCode == 200) {
+    return jsonDecode(response.body);
+  } else {
+    throw Exception('Failed to get the data');
+  }
 }
