1507ec9aa658a5a13162ba78780e14d5e85160ef
devSomethingSomething <61376074+devSomethingSomething@users.noreply.github.com>
2021-06-02 17:27:04 +0200

    Add initial project files

---
+++ dart_async_practice/bin/dart_async_practice.dart
@@ -0,0 +1,5 @@
+import 'package:dart_async_practice/dart_async_practice.dart' as dart_async_practice;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_async_practice.calculate()}!');
+}
---
+++ dart_async_practice/test/dart_async_practice_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_async_practice/dart_async_practice.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
